30343a0cdcc7b4a9fd2da3e59e5579028fd5fc8f
Stytan <stytanus@gmail.com>
2017-05-05 17:07:22 +0300

    Create Birmingham.cs

---
+++ Population/Birmingham.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Population
+{
+	namespace UnitedKingdom
+	{
+		/// <summary>
+		/// Description of Birmingham.
+		/// </summary>
+		public class Birmingham : City
+		{
+			public Birmingham()
+			{
+				name = "Birmingham";
+				country = "United Kingdom";
+				population = 1111300;
+			}
+		}
+	}
+}
