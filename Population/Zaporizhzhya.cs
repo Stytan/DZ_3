6b523741b024c89ca614694fbe30afcc4006967d
Stytan <stytanus@gmail.com>
2017-05-05 17:08:15 +0300

    Create Zaporizhzhya.cs

---
+++ Population/Zaporizhzhya.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Population
+{
+	namespace Ukraine
+	{
+		/// <summary>
+		/// Description of Zaporizhzhya.
+		/// </summary>
+		public class Zaporizhzhya : City
+		{
+			public Zaporizhzhya()
+			{
+				name = "Zaporizhzhya";
+				country = "Ukraine";
+				population = 757650;
+			}
+		}
+	}
+}
