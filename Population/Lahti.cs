bfed8dc239c2460574822f26f1d04bc774b79020
Stytan <stytanus@gmail.com>
2017-05-05 17:07:50 +0300

    Create Lahti.cs

---
+++ Population/Lahti.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Population
+{
+	namespace Finland
+	{
+		/// <summary>
+		/// Description of Lahti.
+		/// </summary>
+		public class Lahti : City
+		{
+			public Lahti()
+			{
+				name = "Lahti";
+				country = "Finland";
+				population = 118885;
+			}
+		}
+	}
+}
