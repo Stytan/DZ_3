e37e3abbfb5c50890186855b4e50fecb71c9b291
Stytan <stytanus@gmail.com>
2017-05-05 17:06:04 +0300

    1

--- Population/Program.cs
+++ Population/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Population
 {
@@ -6,6 +6,21 @@ namespace Population
     {
         static void Main(string[] args)
         {
+			City cityLakhti = new Finland.Lahti();
+			City cityZaporizhzhya = new Ukraine.Zaporizhzhya();
+			City cityBirmingham = new UnitedKingdom.Birmingham();
+			Console.WriteLine(cityLakhti);
+			Console.WriteLine(cityZaporizhzhya);
+			Console.WriteLine(cityBirmingham);
+			Console.WriteLine("{0} < {1} = {2}", cityLakhti.Name, cityZaporizhzhya.Name, cityLakhti < cityZaporizhzhya);
+			Console.WriteLine("{0} > {1} = {2}", cityLakhti.Name, cityZaporizhzhya.Name, cityLakhti > cityZaporizhzhya);
+			Console.WriteLine("{0} < {1} = {2}", cityBirmingham.Name, cityZaporizhzhya.Name, cityBirmingham < cityZaporizhzhya);
+			Console.WriteLine("{0} > {1} = {2}", cityBirmingham.Name, cityZaporizhzhya.Name, cityBirmingham > cityZaporizhzhya);
+			Console.WriteLine("{0} == {1} = {2}", cityLakhti.Name, cityZaporizhzhya.Name, cityLakhti == cityZaporizhzhya);
+			Console.WriteLine("{0} != {1} = {2}", cityZaporizhzhya.Name, cityZaporizhzhya.Name, !cityZaporizhzhya.Equals(cityZaporizhzhya));
+			Console.WriteLine("{0} == {1} = {2}", cityZaporizhzhya.Name, cityZaporizhzhya.Name, cityZaporizhzhya == cityZaporizhzhya);
+        	Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
         }
     }
 }
