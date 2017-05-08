d1623f680b8effc17ecda9fcaf9cfe3162a49e2d
Stytan <stytanus@gmail.com>
2017-05-08 15:56:20 +0300

    Update Wonder.cs

--- SevenWonders/Wonder.cs
+++ SevenWonders/Wonder.cs
@@ -1,61 +1,67 @@
-﻿using System;
+using System;
 
 namespace SevenWonders
 {
-    /// <summary>
-    /// Description of Wonder.
-    /// </summary>
-    public abstract class Wonder
-    {
-        protected string name;
-        public virtual string Name
-        {
-            get
-            {
-                return name;
-            }
-        }
+	/// <summary>
+	/// Класс "чудо света"
+	/// </summary>
+	public abstract class Wonder
+	{
+		/// <summary>
+		/// Название чуда
+		/// </summary>
+		protected string name;
+		public virtual string Name {
+			get {
+				return name;
+			}
+		}
+		
+		/// <summary>
+		/// Место расположения
+		/// </summary>
+		protected string location;
+		public virtual string Location {
+			get {
+				return location;
+			}
+		}
+		
+		/// <summary>
+		/// Создатели
+		/// </summary>
+		protected string builders;
+		public virtual string Builders {
+			get {
+				return builders;
+			}
+		}
 
-        protected string location;
-        public virtual string Location
-        {
-            get
-            {
-                return location;
-            }
-        }
+		/// <summary>
+		/// Описание
+		/// </summary>
+		private string description;
+		public virtual string Description {
+			get {
+				return description;
+			}
+			set {
+				description = value;
+			}
+		}
 
-        protected string builders;
-        public virtual string Builders
-        {
-            get
-            {
-                return builders;
-            }
-        }
+		/// <summary>
+		/// Показ чуда
+		/// </summary>
+		public abstract void Show();
 
-        private string description;
-        public virtual string Description
-        {
-            get
-            {
-                return description;
-            }
-            set
-            {
-                description = value;
-            }
-        }
-
-        public abstract void Show();
-
-        public override string ToString()
-        {
-            return (string)(
-                "Name of wonder: " + Name +
-                "\nLocation: " + Location +
-                "\nBuilders: " + Builders +
-                "\nDescription: " + Description);
-        }
-    }
+		public override string ToString()
+		{
+			return (string)(
+			    "Name of wonder: " + Name +
+			    "\nLocation: " + Location +
+			    "\nBuilders: " + Builders +
+			    "\nDescription: " + Description);
+		}
+	}
 }
