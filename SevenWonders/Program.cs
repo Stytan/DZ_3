8398e76205c0a5ea930e47f0065c30bab7887b15
Stytan <stytanus@gmail.com>
2017-05-08 15:54:56 +0300

    Update Program.cs

--- SevenWonders/Program.cs
+++ SevenWonders/Program.cs
@@ -1,24 +1,26 @@
-﻿using System;
+using System;
 using SevenWonders;
 
 namespace DZ_3
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Wonder[] Wonders = { new GreatPyramidOfGiza(),
-                                new HangingGardensOfBabylon(),
-                                new TempleOfArtemisAtEphesus(),
-                                new StatueOfZeusAtOlympia(),
-                                new MausoleumAtHalicarnassus(),
-                                new ColossusOfRhodes(),
-                                new LighthouseOfAlexandria()};
-            foreach (Wonder w in Wonders)
-            {
-                Console.WriteLine("\n\n"+w);
-                w.Show();
-            }
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Wonder[] Wonders = { new GreatPyramidOfGiza(),
+				new HangingGardensOfBabylon(),
+				new TempleOfArtemisAtEphesus(),
+				new StatueOfZeusAtOlympia(),
+				new MausoleumAtHalicarnassus(),
+				new ColossusOfRhodes(),
+				new LighthouseOfAlexandria()
+			};
+			foreach (Wonder w in Wonders) {
+				Console.WriteLine("\n\n" + w);
+				w.Show();
+			}
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
 }
