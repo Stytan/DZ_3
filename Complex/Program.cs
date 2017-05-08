f618758ccf251be60e250d00556d6d46296a805e
Stytan <stytanus@gmail.com>
2017-05-08 15:59:31 +0300

    Update Program.cs

--- Complex/Program.cs
+++ Complex/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace Complex
 {
@@ -10,6 +6,12 @@ namespace Complex
     {
         static void Main(string[] args)
         {
+			cmplx z = new cmplx(1, 1);
+			cmplx z1;
+			z1 = z - (z * z * z - 1) / (3 * z * z);
+			Console.WriteLine("z1 = {0}", z1);
+			Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
         }
     }
 }
